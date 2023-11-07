9d80eca9efa78a2a61ff9c420ead38bc395ba838
Santhaseelan K <143274775+santhaseelan03@users.noreply.github.com>
2023-11-07 10:16:48 +0530

    Delete Web-Size directory

--- Web-Size/Assets/src/script.js
+++
@@ -1,8 +0,0 @@
-// navber
-
-const toggleButton = document.getElementById("toggleButton");
-const navLinks = document.getElementById("navLinks");
-
-toggleButton.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
